9c011037371773e5f283836ad1698fa0a02225c7
LYC <newvry@hotmail.com>
2017-06-14 10:00:55 +0800

    Routes: build products routes and change root to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
+  root "products#index"
+  
   namespace :admin do 
     resources :products
   end
+
+  resources :products
 
 end
