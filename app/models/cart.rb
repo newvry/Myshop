43e9cae73629e27c14a640e12cfff90ae43fc85d
LYC <newvry@hotmail.com>
2017-06-14 21:26:18 +0800

    Create cart/cart_item and build association many to many

---
+++ app/models/cart.rb
@@ -0,0 +1,9 @@
+class Cart < ApplicationRecord
+
+  has_many :cart_items, dependent: :destroy
+  has_many :items ,through: :cart_item, source: :product
+  #當關聯名稱不一致的時候，可以加上source指名是哪一種物件，source需搭配through一起使用，
+  #@cart.items可以列出這台車上所有item的資料
+  #@cart.cart_items可以列出所有cart_item的資料
+
+end
